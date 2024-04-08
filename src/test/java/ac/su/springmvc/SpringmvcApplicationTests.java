e6b06efaa69759fbc2dedbb89d10d83aa173973f
wild-mental <wild.mental@kidsnote.com>
2024-04-08 11:26:09 +0900

    initial commit

---
+++ src/test/java/ac/su/springmvc/SpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
