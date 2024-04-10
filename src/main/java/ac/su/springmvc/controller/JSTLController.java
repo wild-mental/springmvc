a9f12c15225be78f9f3668de62d979fcf69af076
wild-mental <wild.mental@kidsnote.com>
2024-04-10 15:54:37 +0900

    JSTL fmt - i18n 처리 코드 삭제

--- src/main/java/ac/su/springmvc/controller/JSTLController.java
+++ src/main/java/ac/su/springmvc/controller/JSTLController.java
@@ -21,17 +21,4 @@ public class JSTLController {
         model.addAttribute("message", "this is long message");
         return "jstl/example";
     }
-
-    @Autowired
-    private MessageSource messageSource;
-
-    @GetMapping("/i18n-msg")
-    public String getMessage(@RequestParam(name = "lang", defaultValue = "en") String lang, Model model) {
-        Locale locale = new Locale(lang);
-        String message = messageSource.getMessage("hello.message", null, locale);
-        // 아래 메시지 정상 출력 확인
-        // System.out.println(message);
-        model.addAttribute("helloMessage", message);
-        return "jstl/i18n-msg";
-    }
 }
