32063fcfa13fdf03bc88867a56b102d38ea002f0
wild-mental <wild.mental@kidsnote.com>
2024-04-11 00:52:52 +0900

    JSTLController import 최적화

--- src/main/java/ac/su/springmvc/controller/JSTLController.java
+++ src/main/java/ac/su/springmvc/controller/JSTLController.java
@@ -1,16 +1,10 @@
 package ac.su.springmvc.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Encoding;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
-import java.util.Locale;
 
 @Controller
 @RequestMapping("/jstl")
