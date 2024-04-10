5c3835d8dd2b70d0ba3c3f17380f20550921970d
wild-mental <wild.mental@kidsnote.com>
2024-04-10 21:24:34 +0900

    Annotation을 사용한 HTTP 메서드별 엔드포인트 생성 예제

---
+++ src/main/java/ac/su/springmvc/controller/MethodAnnotationSampleController.java
@@ -0,0 +1,50 @@
+package ac.su.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/method-annotation")
+public class MethodAnnotationSampleController {
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String get() {
+        return "this is GET method sample";
+    }
+
+    @GetMapping("/get2")
+    @ResponseBody
+    public String get2() {
+        return "this is GET method sample 2";
+    }
+
+    @PostMapping("/post")
+    @ResponseBody
+    public String post() {
+        return "this is POST method sample";
+    }
+
+    @PutMapping("/put")
+    @ResponseBody
+    public String put() {
+        return "this is PUT method sample";
+    }
+
+    @DeleteMapping("/delete")
+    @ResponseBody
+    public String delete() {
+        return "this is DELETE method sample";
+    }
+
+    @PatchMapping("/patch")
+    @ResponseBody
+    public String patch() {
+        return "this is PATCH method sample";
+    }
+
+    @PatchMapping("/patch2")
+    @ResponseBody
+    public String patch2() {
+        return "this is PATCH method sample 2";
+    }
+}
