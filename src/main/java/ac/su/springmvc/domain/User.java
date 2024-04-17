771c0041cb27bc91b7a08d05b7650afdee655c45
wild-mental <wild.mental@kidsnote.com>
2024-04-18 03:46:45 +0900

    users 테이블명 단수형 user로 변경

--- src/main/java/ac/su/springmvc/domain/User.java
+++ src/main/java/ac/su/springmvc/domain/User.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-@Table(name = "users")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
