ee44f68a3a0c01048ce0e4cc58ffabf82f31b14c
wild-mental <wild.mental@kidsnote.com>
2024-04-18 03:13:01 +0900

    DTO 소개 - (1) Entity 수정 사례 2

--- src/main/java/ac/su/springmvc/domain/User.java
+++ src/main/java/ac/su/springmvc/domain/User.java
@@ -19,10 +19,11 @@ public class User {
     private String email;
 
     //1) DB 변경 후, 아래와 같은 컬럼이 비즈니스 로직상으로는 필요하지만
-    //   불필요하게 모든 응답에 표현되는 것을 막고 싶음 -> DTO 사용
-//    @Column(nullable = false)
-//    private double weight;
-//
-//    @Column(nullable = false)
-//    private double height;
+    //   불필요하게 모든 응답에 표현되는 것을 막고 싶음
+    //   -> DTO 사용으로 해결!
+    @Column(nullable = false)
+    private double weight;
+
+    @Column(nullable = false)
+    private double height;
 }
